5bce601c4b22e738a6cabb2ad46eab6d1c7517b0
Betsi Loayza <betsi18_12@hotmail.com>
2017-06-12 12:06:57 -0500

    filtro sedes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
 var i=0;
+var j=0;
 const cargarfoto = (i)=>{
-  $('#fotos').empty();
-  const fotos = $('<img src="fotos/peru/'+encodeURI(peru[i].image)+'">');
-  $('#fotos').append(fotos);
+   $('#fotos').empty();
+   const fotos = $('<img src="fotos/peru/'+encodeURI(peru[i].image)+'">');
+   $('#fotos').append(fotos);
 }
+
+const cargarfotomexico = (j)=>{
+   $('#fotos').empty();
+   const fotosmexico = $('<img src="fotos/mexico/'+encodeURI(mexico[j].image)+'">');
+   $('#fotos').append(fotosmexico);
+}
+
+$("#sedes").change(function(){
+if($('select[id=sedes]').val()=='lima'){
+  cargarfoto(0);
+  $('.comprobar').click(function(){
+    if ($('#inputNombre').val()==peru[i].name && i<peru.length){
+      i = i+1;
+      cargarfoto(i);
+    }else{
+      $('#inputNombre').val()== "Termino juego";
+    }
+    console.log(peru[i].name);
+    // $('#inputNombre').empty();
+  });
+} else {
+  cargarfotomexico(0);
+  $('.comprobar').click(function(){
+    if ($('#inputNombre').val()==mexico[j].name){
+      j = j+1;
+      cargarfotomexico(j);
+    }
+    console.log(mexico[j].name);
+  });
+}
+
+});
+
+
+
+
+
 
 if($('#sedes').val()=="lima"){
   cargarfoto(0);
@@ -13,6 +51,7 @@ if($('#sedes').val()=="lima"){
       cargarfoto(i);
       console.log(peru[i].name);
     }
+    $('#inputNombre').empty();
   });
 }
 console.log(fotos);
